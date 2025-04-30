0c189bdea3b13f1f09085b39e70a99c66a1a1738
Martin Justa <mjusta@c2r8s5.42prague.com>
2025-04-30 09:56:44 +0200

    add C00

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/10 18:08:37 by mjusta            #+#    #+#             */
+/*   Updated: 2025/04/10 18:13:17 by mjusta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	int	i;
+
+	i = 'z';
+	while (i >= 'a')
+	{
+		write(1, &i, 1);
+		i--;
+	}
+}
+/*
+int	main()
+{
+	ft_print_reverse_alphabet();
+	return 0;
+}*/
