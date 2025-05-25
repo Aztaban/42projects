203e4f5bcc655884a028b487e2c31aafbd3cb0fb
Aztaban <Aztabanee@gmail.com>
2025-05-25 23:44:01 +0200

    add memchr test

--- core/libft/ft_strncmp.c
+++ core/libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 18:04:35 by mjusta            #+#    #+#             */
-/*   Updated: 2025/05/25 19:37:38 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/05/25 23:40:02 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t			i;
 
 	i = 0;
-	while (i < n && s1[i] == s2[i]) 
+	while (i < n && s1[i] == s2[i])
 	{
 		if (s1[i] == '\0')
 			return (0);
--- core/libft/ft_strnstr.c
+++ core/libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 19:52:40 by mjusta            #+#    #+#             */
-/*   Updated: 2025/05/25 19:53:42 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/05/25 23:39:41 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	
+	size_t	i;
+	size_t	j;
 
+	if (!*needle)
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && (i + j < len))
+			j++;
+		if (!needle[j])
+			return ((char *)(haystack + i));
+		i++;
+	}
+	return (NULL);
 }
