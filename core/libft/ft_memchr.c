471b099a32859f07f7f2c51831714408d6ef7067
Martin Justa <mjusta@c2r7s7.42prague.com>
2025-05-26 12:45:04 +0200

    add new functions

---
+++ core/libft/ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 11:46:18 by mjusta            #+#    #+#             */
+/*   Updated: 2025/05/26 12:16:08 by mjusta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*str;
+
+	str = s;
+	while (n--)
+	{
+		if (*str == (unsigned char)c)
+			return (void *)str;
+		str++;
+	}
+	return (NULL);
+}
