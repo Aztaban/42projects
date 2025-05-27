40b2bab3067954be36f47327fa6b0f1de52fb001
Martin Justa <mjusta@c2r7s7.42prague.com>
2025-05-27 18:49:39 +0200

    fixed casting

--- core/libft/ft_memcpy.c
+++ core/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 11:45:23 by mjusta            #+#    #+#             */
-/*   Updated: 2025/05/25 17:19:21 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/05/27 18:41:08 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	const unsigned char	*s;
 	size_t				i;	
 
-	d = dest;
-	s = src;
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
 	i = 0;
 	while (i < n)
 	{
