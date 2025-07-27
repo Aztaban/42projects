147198b6ff5d1dfb1986c5664e538ce2ce724d26
Martin Justa <Aztabanee@gmail.com>
2025-07-27 13:52:56 +0200

    Removed all submodules from core/

--- piscine/shell01/ex07/r_dwssap.sh
+++
@@ -1,8 +0,0 @@
-cat /etc/passwd \
-  | grep -v '^#' \
-  | awk 'NR % 2 == 0' \
-  | cut -d: -f1 \
-  | rev \
-  | sort -r \
-  | sed -n "${FT_LINE1},${FT_LINE2}p" \
-  | awk '{printf "%s, ", $0} END {printf "\b\b."}'
